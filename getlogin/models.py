b21261e568bc73097abfd4435accf54f12d71109
yan <chungyan5@gmail.com>
2015-03-27 21:31:18 +0800

    - miss the friendly wording in Email address and Password fields - css absolute layout

--- getlogin/models.py
+++ getlogin/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class LoginPair(models.Model):
-        email = models.CharField(max_length=200)
-        pw = models.CharField(max_length=200)
+        email = models.CharField(max_length=200,
+                                 verbose_name="Amonics Email",
+                                 help_text="Please input your full email address as XXX@amonics.com",
+                                 )
+        pw = models.CharField(max_length=200,
+                              verbose_name="Password",
+                              )
         newPw = models.CharField(max_length=200)
 
 	def __unicode__(self):
